03c4678a3f2187e9bf77dc881e65f4d595495c14
afwyou <afwyousenge@gmail.com>
2021-09-04 09:44:32 +0800

    載入express-session and bodyParser

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var bodyParser = require('body-parser')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var session = require('express-session')
+
 
 var app = express();
 
@@ -15,6 +17,14 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 100 * 1000 }
+}))
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
